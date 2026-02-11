161f5c285ec548a20d80fcc8e2ad6ff1f8fa8941
Dibin Sunuwar <dibinsunuwar7@gmail.com>
2026-02-10 19:11:29 -0600

    calculator

---
+++ calculatorapp.py
@@ -0,0 +1,17 @@
+a = float(input("enter the number: "))
+operator = input("enter the operator (+, -, *, /): ")
+b = float(input("enter the number: "))
+if operator == "+":
+    result = a + b
+elif operator == "-":
+    result = a - b
+elif operator == "*":
+    result = a * b
+elif operator == "/":
+    if b != 0:
+        result = a / b
+    else:
+        print(f"error: the number is not divisible by zero")
+else:
+    print("invalid operator! use +, -, *, /")
+print(f"result:{result:.2f}")
